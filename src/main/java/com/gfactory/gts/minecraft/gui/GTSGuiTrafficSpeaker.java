ec356a233dff91fc4ba214dacefceb3cc99952ad
Gingarenpo <gingarenpo.9844@gmail.com>
2026-08-13 20:13:23 +0900

    チャンネル名を変更できるようにGUI調整

--- src/main/java/com/gfactory/gts/minecraft/gui/GTSGuiTrafficSpeaker.java
+++ src/main/java/com/gfactory/gts/minecraft/gui/GTSGuiTrafficSpeaker.java
@@ -1,16 +1,26 @@
 package com.gfactory.gts.minecraft.gui;
 
 import com.gfactory.gts.minecraft.GTS;
+import com.gfactory.gts.minecraft.network.packet.GTSPacketTileEntity;
 import com.gfactory.gts.minecraft.tileentity.GTSTileEntity;
 import com.gfactory.gts.minecraft.tileentity.GTSTileEntityTrafficSpeaker;
 import com.gfactory.gts.pack.GTSPack;
 import com.gfactory.gts.pack.config.GTSConfig;
 import com.gfactory.gts.pack.config.GTSTrafficSpeakerConfig;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiTextField;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.nbt.NBTTagCompound;
 
+import java.io.IOException;
 import java.util.Map;
 import java.util.TreeMap;
 
+/**
+ * 交通信号機のモデル選択や、チャンネルの設定を行う
+ */
 public class GTSGuiTrafficSpeaker extends GTSGuiModelChoose<GTSTileEntityTrafficSpeaker> {
+
     /**
      * 必ずTileEntityを渡す必要がある
      *
@@ -20,8 +30,36 @@ public class GTSGuiTrafficSpeaker extends GTSGuiModelChoose<GTSTileEntityTraffic
         super(tileEntity);
     }
 
+    /**
+     * チャンネルの名前を保持するフィールド
+     */
+    private GuiTextField channelName;
+
     @Override
-    public TreeMap<String, ? extends GTSConfig> getModelElements() {
+    public void initGui() {
+        super.initGui();
+
+        // チャンネルの設定を行うテキストボックスを作成
+        this.channelName = new GuiTextField(0, fontRenderer, this.width / 2 + MARGIN, fontRenderer.FONT_HEIGHT * 2 + MARGIN * 2, this.width / 2 - MARGIN * 2, fontRenderer.FONT_HEIGHT);
+        this.channelName.setMaxStringLength(32);
+        this.channelName.setFocused(true);
+        this.channelName.setText(((GTSTileEntityTrafficSpeaker) tileEntity).getChannel());
+
+        // その下にボタンを作成
+        this.addButton(new GuiButton(1, this.width / 2 + MARGIN, fontRenderer.FONT_HEIGHT * 3 + MARGIN * 3, I18n.format("gts.gui.channel.apply")));
+    }
+
+    @Override
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        super.drawScreen(mouseX, mouseY, partialTicks);
+
+        // 右側パネル2行目から、ラベルを配置
+        fontRenderer.drawString(I18n.format("gts.gui.channel.name"), this.width / 2 + MARGIN, fontRenderer.FONT_HEIGHT + MARGIN, 0xFFFFFF);
+        this.channelName.drawTextBox();
+    }
+
+    @Override
+    public TreeMap<String, GTSTrafficSpeakerConfig> getModelElements() {
         TreeMap<String, GTSTrafficSpeakerConfig> elements = new TreeMap<>();
         for (GTSPack p: GTS.LOADER.getPacks()) {
             for (Map.Entry<String, GTSConfig<GTSConfig.GTSTexture>> e: p.getConfigs().entrySet()) {
@@ -31,5 +69,40 @@ public class GTSGuiTrafficSpeaker extends GTSGuiModelChoose<GTSTileEntityTraffic
             }
         }
         return elements;
+    }
+
+    @Override
+    protected void keyTyped(char typedChar, int keyCode) throws IOException {
+        this.channelName.textboxKeyTyped(typedChar, keyCode);
+        super.keyTyped(typedChar, keyCode);
+    }
+
+    @Override
+    protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
+        this.channelName.mouseClicked(mouseX, mouseY, mouseButton);
+        super.mouseClicked(mouseX, mouseY, mouseButton);
+    }
+
+    @Override
+    public void updateScreen() {
+        super.updateScreen();
+        this.channelName.updateCursorCounter();
+    }
+
+    @Override
+    protected void actionPerformed(GuiButton button) throws IOException {
+        if (button.id == 1) {
+            // チャンネル名を更新する
+            GTSTileEntityTrafficSpeaker te = (GTSTileEntityTrafficSpeaker) tileEntity;
+            te.setChannel(this.channelName.getText());
+            GTS.NETWORK.sendToServer(new GTSPacketTileEntity<>(te.writeToNBT(new NBTTagCompound()), te.getPos(), GTSTileEntityTrafficSpeaker.class));
+            te.markDirty();
+            te.getWorld().notifyBlockUpdate(
+                    te.getPos(),
+                    te.getWorld().getBlockState(te.getPos()),
+                    te.getWorld().getBlockState(te.getPos()),
+                    3);
+        }
+        super.actionPerformed(button);
     }
 }
