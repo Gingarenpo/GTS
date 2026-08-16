74587a5bd722ab8aed69f6b5b594321e788cad75
Gingarenpo <gingarenpo.9844@gmail.com>
2026-08-16 13:39:18 +0900

    alpha5として凍結

--- src/main/java/com/gfactory/gts/minecraft/GTS.java
+++ src/main/java/com/gfactory/gts/minecraft/GTS.java
@@ -39,7 +39,7 @@ public class GTS {
     /**
      * Modのバージョン
      */
-    public static final String VERSION = "2.0-alpha4";
+    public static final String VERSION = "2.0-alpha5";
 
     /**
      * GTS Logger
